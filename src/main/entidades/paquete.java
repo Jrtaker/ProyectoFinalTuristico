dc2033415a62f4926acf27affa7644bf27e56ad4
YadhiPrograma <yadhiprogramacion@gmail.com>
2023-10-03 11:47:15 -0300

    paquete entidades
    solucion de errores

--- src/main/entidades/paquete.java
+++ src/main/entidades/paquete.java
@@ -12,12 +12,14 @@ private Pasaje pasaje;
 public paquete(){
     
 }
-    public paquete(int idpaquete, Ciudad origen, Ciudad destino, Alojamiento alojamiento, Pasaje pasaje )
+    public paquete(int idpaquete, Ciudad origen, Ciudad destino, Alojamiento alojamiento, Pasaje pasaje ){
     this.idPaquete = idPaquete;
     this.origen = origen;
     this.destino= destino; 
+    this.alojamiento =alojamiento;
+    this.pasaje = pasaje;
     
-
+}
 
 
 }
